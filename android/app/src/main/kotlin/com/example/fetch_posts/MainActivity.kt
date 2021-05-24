154c8d09f881c90b9af0ee39ff292fb1f229da00
zaid_sameer <amrikee7@gmail.ocm>
2021-05-24 21:59:07 +0300

    Add all my files

---
+++ android/app/src/main/kotlin/com/example/fetch_posts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fetch_posts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _makeGetRequest() async {
+    // make GET request
+    String url = 'https://jsonplaceholder.typicode.com/posts';
+    final response = await http.get(url);
+    int statusCode = response.statusCode;
+
+    String json = response.body;
+    print(statusCode);
+    print(json);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _makeGetRequest();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
